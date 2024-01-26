af70f0cbc5f4c3163f0239a265db00acfe15bd29
jonathanwoo <jwhwoo@gmail.com>
2024-01-26 10:10:50 -0800

    update packages

--- src/components/common/SiteNav.js
+++ src/components/common/SiteNav.js
@@ -2,7 +2,11 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
-function SiteNav() {
+function SiteNav(props) {
+    const handleLogout = () => {
+        props.logOut();
+    }
+
     return (
         <header>
             <Navbar bg="dark" variant="dark" expand="lg">
@@ -11,8 +15,7 @@ function SiteNav() {
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-md-auto">
-                    <Nav.Link href="/login">Login</Nav.Link>
-                    <Nav.Link href="/register">Register</Nav.Link>
+                        <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
                     </Nav>
                     </Navbar.Collapse>
                 </Container>
