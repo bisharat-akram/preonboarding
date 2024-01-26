ffec867f30946a6ed6b5867ede9de643342e4e97
jonathanwoo <jwhwoo@gmail.com>
2024-01-26 11:12:52 -0800

    amplify update

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "aiagentplatformgraphqlapi": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "aiagentplatform2bc895b2": {
       "AppClientID": "string",
@@ -8,6 +15,12 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolArn": "string",
       "UserPoolId": "string",
       "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "aiagentplatformcontactstorage": {
+      "BucketName": "string",
+      "Region": "string"
     }
   }
 }
--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -1,4 +1,7 @@
 import { useState } from 'react';
+import { API, graphqlOperation, Storage } from 'aws-amplify';
+
+import { createContact } from '../../graphql/mutations';
 
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
@@ -7,10 +10,35 @@ import Card from 'react-bootstrap/Card';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
+import {v4 as uuid} from 'uuid';
+
 function Contacts() {
     //const [contacts, setContacts] = useState([]);
     const [contactData, setContactData] = useState({name: "", email: "", cell: ""});
+    const [profilePic, setProfilePic] = useState("");
 
+    const addNewContact = async () => {
+        try {
+            const { name, email, cell } = contactData;
+
+            // Upload pic to S3
+            Storage.configure({ region: 'us-west-1' });
+            const { key } = await Storage.put(`${uuid()}.png`, profilePic, {contentType: 'image/png'});
+
+            const newContact = {
+                id: uuid(),
+                name,
+                email,
+                cell,
+                profilePicPath: key
+            };
+
+            // Persist new Contact
+            await API.graphql(graphqlOperation(createContact, {input: newContact}));
+        } catch(err) {
+            console.log('error', err);
+        }
+    }
 
     return (
         <Container>
@@ -57,8 +85,8 @@ function Contacts() {
                         </Form.Group>
                         <Form.Group className="mb-3" controlId="formBasicText">
                             <Form.Label>Profile Pic</Form.Label>
-                            <Form.Control type="file" accept="image/png" />
-                                          //onChange={evt => setProfilePic(evt.target.files[0])} />
+                            <Form.Control type="file" accept="image/png"
+                                          onChange={evt => setProfilePic(evt.target.files[0])} />
                         </Form.Group>
                         <Button variant="primary" type="button" onClick={addNewContact}>Add Contact &gt;&gt;</Button>&nbsp;                        
                     </Form>
