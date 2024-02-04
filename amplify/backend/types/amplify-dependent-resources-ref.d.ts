d886d4f6666616ec1e132e54e8291dcafdb76d5b
neo.lee <neo.lee@rambunct.com>
2024-02-04 19:05:50 +0000

    readd amplify

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,26 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "aiagentplatformgraphqlapi": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "aiagentplatform2bc895b2": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "aiagentplatformcontactstorage": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
