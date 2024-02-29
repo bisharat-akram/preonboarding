e5e198d9a58f148fc6056c613f5fad6375dad41a
Md Nazmul Hasan <farhapartex@gmail.com>
2024-03-01 00:36:56 +0600

    Added menus to topbar

--- src/components/common/SiteNav.js
+++ src/components/common/SiteNav.js
@@ -11,12 +11,14 @@ function SiteNav(props) {
         <header>
             <Navbar bg="dark" variant="dark" expand="lg">
                 <Container>
-                <Navbar.Brand>Prometheus AGI ML App</Navbar.Brand>
+                    <Navbar.Brand>Contacts App</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ms-md-auto">
-                        <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
-                    </Nav>
+                        <Nav className="ms-md-auto">
+                            <Nav.Link href="/">Upload & View Files</Nav.Link>
+                            <Nav.Link href="/">Results</Nav.Link>
+                            <Nav.Link onClick={handleLogout} style={{ color: '#ff3333' }}>Logout</Nav.Link>
+                        </Nav>
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
