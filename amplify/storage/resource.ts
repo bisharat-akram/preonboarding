c828b34afd0a6a14d1de69cac9641d6d7c70fda7
Harshit Gupta <harshitgupta@Harshits-MacBook-Pro-2.local>
2024-07-25 23:53:50 +0530

    s3 fix

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,10 +1,15 @@
 import { defineStorage } from '@aws-amplify/backend';
 
 export const storage = defineStorage({
-    name: 'filestorage',
+    name: 'filestorages',
     access: (allow) => ({
         'files/{entity_id}/*': [
             allow.guest.to(['read']),
+            allow.entity('identity').to(['read', 'write', 'delete'])
+        ],
+        'files-submissions/{user_sub}/*': [
+            allow.authenticated.to(['read', 'write']),
+            allow.guest.to(['read', 'write']),
             allow.entity('identity').to(['read', 'write', 'delete'])
         ],
         'files-submissions/*': [
